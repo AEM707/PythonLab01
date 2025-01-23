d04eba9950f464c40ea9fbdaa327e214bab31223
Miranda <amiranda267@mycr.redwoods.edu>
2025-01-22 16:42:02 -0800

    finished lab 1

---
+++ lab1.py
@@ -0,0 +1,31 @@
+import math
+
+A = math.pi * 5**2
+V = (4/3) * math.pi * 3**3
+a_side = 3
+b_side = 4
+c_side= a_side**2 + b_side**2
+pyt= math.sqrt(c_side)
+print(A,V,pyt)
+
+first_name = "Andy"
+last_name = "Miranda"
+full_name = first_name + " " + last_name
+name_len =len(full_name)
+print(name_len, full_name, full_name.upper(), full_name.lower())
+
+age = 23
+height = 6.0
+weight = 180
+inches = height * 12
+print(type(age), type(weight), type(height))
+bmi = (weight / inches**2) * 703
+print(bmi)
+
+
+
+
+
+
+
+
